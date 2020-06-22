83ad43842c60a4813cd69750b4b5e222a1387053
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-06-22 13:25:13 -0300

    working on table values, issue remain in ci, cv

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import AdSense from 'react-adsense';
 
 function App() {
 
-    let sex, age, weight, height, results;
+    let sex, age, weight, height;
+    let [results, setResults] = useState({});
+    let [realPeak, setRealPeak] = useState(0);
+    let [realPi, setRealPi] = useState(0);
+    let [realPe, setRealPe] = useState(0);
+    let [realCi, setRealCi] = useState(0);
+    let [realCv, setRealCv] = useState(0);
 
     function calculatePeakFlow() {
         let height_factor = Math.round(height / 5);
@@ -111,7 +117,7 @@ function App() {
         calcci = 2600;
         calccv = calculateCV(pideal);
 
-        results = { calcflow, calcpimax, calcpemax, calcci, calccv, pideal };
+        setResults({ calcflow, calcpimax, calcpemax, calcci, calccv, pideal });
 
         document.getElementById("calcflow").innerHTML = calcflow.toFixed(2);
         document.getElementById("calcpimax").innerHTML = calcpimax.toFixed(2);
@@ -119,8 +125,130 @@ function App() {
         document.getElementById("calcci").innerHTML = calcci.toFixed(2);
         document.getElementById("calccv").innerHTML = calccv.toFixed(2);
         document.getElementById("pideal").innerHTML = pideal.toFixed(2);
+
+        document.getElementById("calcflowTarget").innerHTML = `Maior que ${(0.8 * calcflow).toFixed(2)}`;
+        document.getElementById("calcpimaxTarget").innerHTML = `Maior ou igual a ${(0.8 * calcpimax).toFixed(2)}`;
+        document.getElementById("calcpemaxTarget").innerHTML = `Maior ou igual a ${(0.8 * calcpemax).toFixed(2)}`;
+        document.getElementById("calcciTarget").innerHTML = `Maior ou igual a ${(0.8 * calcci).toFixed(2)}`;
+        document.getElementById("calccvTarget").innerHTML = `Maior que ${(0.8 * calccv).toFixed(2)}`;
+        document.getElementById("pidealTarget").innerHTML = `${(pideal - weight).toFixed(2)}`;
+
+        handlePidealCondition(pideal - weight);
+
     };
 
+    function handlePidealCondition(value) {
+        let pidealConditionValue;
+
+        if (value == 0) {
+            pidealConditionValue = "No Peso";
+        } else if (value > 0) {
+            pidealConditionValue = "Abaixo do Peso";
+        } else {
+            pidealConditionValue = "Acima do Peso";
+        }
+
+        document.getElementById("pidealCondition").innerHTML = pidealConditionValue;
+    }
+            
+
+
+    function handlePeakChange(event) {
+        const realpeakvalue = event.target.value;
+
+        setRealPeak(realpeakvalue);
+
+        let zone;
+
+        if (realpeakvalue > 0.79 * results.calcflow) {
+            zone = "Zona Verde";
+        } else if (realpeakvalue > 0.49 * results.calcflow) {
+            zone = "Zona Amarela";
+        } else {
+            zone = "Zona Vermelha";
+        }
+
+        document.getElementById("peakCondition").innerHTML = zone;
+    };
+
+
+    function handlePiChange(event) {
+        const realpivalue = event.target.value;
+
+        setRealPi(realpivalue);
+
+        let zone;
+
+        if (realpivalue > 0.79 * results.calcpimax) {
+            zone = "Normal";
+        } else if (realpivalue > 0.59 * results.calcpimax) {
+            zone = "Fraqueza Leve";
+        } else if (realpivalue > 0.39 * results.calcpimax) {
+            zone = "Fraqueza Moderada";
+        } else if (realpivalue > 0.19 * results.calcpimax) {
+            zone = "Fraqueza Intensa";
+        } else {
+            zone = "Fraqueza Muito Intensa";
+        }
+
+        document.getElementById("piCondition").innerHTML = zone;
+    };
+
+    function handlePeChange(event) {
+        const realpevalue = event.target.value;
+
+        setRealPe(realpevalue);
+
+        let zone;
+
+        if (realpevalue > 0.79 * results.calcpemax) {
+            zone = "Normal";
+        } else if (realpevalue > 0.59 * results.calcpemax) {
+            zone = "Fraqueza Leve";
+        } else if (realpevalue > 0.39 * results.calcpemax) {
+            zone = "Fraqueza Moderada";
+        } else if (realpevalue > 0.19 * results.calcpemax) {
+            zone = "Fraqueza Intensa";
+        } else {
+            zone = "Fraqueza Muito Intensa";
+        }
+
+        document.getElementById("peCondition").innerHTML = zone;
+    };
+
+    function handleCiChange(event) {
+        const realcivalue = event.target.value;
+
+        setRealCi(realcivalue);
+
+        let zone;
+
+        if (realcivalue > 0.79 * results.calcci) {
+            zone = "Normal";
+        } else {
+            zone = "CI Anormal";
+        }
+
+        document.getElementById("ciCondition").innerHTML = zone;
+    };
+
+    function handleCvChange(event) {
+        const realcvvalue = event.target.value;
+
+        setRealCv(realcvvalue);
+
+        let zone;
+
+        if (realcvvalue > 0.59 * results.calccv) {
+            zone = "Leve";
+        } else if (realcvvalue > 0.49 * results.calccv) {
+            zone = "Moderada";
+        } else {
+            zone = "Grave";
+        }
+
+        document.getElementById("cvCondition").innerHTML = zone;
+    };
 
 
   return (
@@ -167,45 +295,45 @@ function App() {
                 </tr>
                 <tr>
                     <th className="tableLeft">Peak Flow</th>
-                    <td className="tableBegin"></td>
-                    <td>Maior que {}</td>
+                    <td className="tableBegin"><input className="tableInput" type="number" onChange={e => handlePeakChange(e)} /></td>
+                    <td id="calcflowTarget"></td>
                     <td id="calcflow"></td>
-                    <td className="tableEnd"></td>
+                    <td className="tableEnd" id="peakCondition"></td>
                 </tr>
                 <tr>
                     <th className="tableLeft">PIMax</th>
-                    <td className="tableBegin"></td>
-                    <td>Maior ou igual a {}</td>
+                    <td className="tableBegin"><input className="tableInput" type="number" onChange={e => handlePiChange(e)} /></td>
+                    <td id="calcpimaxTarget"></td>
                     <td id="calcpimax"></td>
-                    <td className="tableEnd"></td>
+                    <td className="tableEnd" id="piCondition"></td>
                 </tr>
                 <tr>
                     <th className="tableLeft">PEMax</th>
-                    <td className="tableBegin"></td>
-                    <td>Maior ou igual a {}</td>
+                    <td className="tableBegin"><input className="tableInput" type="number" onChange={e => handlePeChange(e)}/></td>
+                    <td id="calcpemaxTarget"></td>
                     <td id="calcpemax"></td>
-                    <td className="tableEnd"></td>
+                    <td className="tableEnd" id="peCondition"></td>
                 </tr>
                 <tr>
                     <th className="tableLeft">CI</th>
-                    <td className="tableBegin"></td>
-                    <td>Maior ou igual a {}</td>
+                    <td className="tableBegin"><input className="tableInput" type="number" onChange={e => handleCiChange(e)}/></td>
+                    <td id="calcciTarget"></td>
                     <td id="calcci"></td>
-                    <td className="tableEnd"></td>
+                    <td className="tableEnd" id="ciCondition"></td>
                 </tr>
                 <tr>
                     <th className="tableLeft">CV</th>
-                    <td className="tableBegin"></td>
-                    <td>Maior que {}</td>
+                    <td className="tableBegin"><input className="tableInput" type="number" onChange={e => handleCvChange(e)}/></td>
+                    <td id="calccvTarget"></td>
                     <td id="calccv"></td>
-                    <td className="tableEnd"></td>
+                    <td className="tableEnd" id="cvCondition"></td>
                 </tr>
                 <tr>
                     <th className="tableLeft">PIdeal</th>
                     <td id="pidealLeft"></td>
-                    <td className="tableBegin"></td>
+                    <td className="tableBegin" id="pidealTarget"></td>
                     <td id="pideal"></td>
-                    <td className="tableEnd"></td>
+                    <td className="tableEnd" id="pidealCondition"></td>
                 </tr>
                 
             </table>
