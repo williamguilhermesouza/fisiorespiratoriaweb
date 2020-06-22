2bf2cd28f0ff0130fd46d699449e949feea86c07
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-06-21 23:10:36 -0300

    Updates

---
+++ precache-manifest.05f29172b0bac3a282b2fd756662f9b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4d724a0fec3de9b6b81210de820da1",
+    "url": "/fisiorespiratoriaweb/index.html"
+  },
+  {
+    "revision": "dc98cbe7825e22153424",
+    "url": "/fisiorespiratoriaweb/static/css/main.3d46eb9f.chunk.css"
+  },
+  {
+    "revision": "f2647c9aee4e2702ecb3",
+    "url": "/fisiorespiratoriaweb/static/js/2.d7a239b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fisiorespiratoriaweb/static/js/2.d7a239b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc98cbe7825e22153424",
+    "url": "/fisiorespiratoriaweb/static/js/main.2029c0c5.chunk.js"
+  },
+  {
+    "revision": "1843c5f29376396547d6",
+    "url": "/fisiorespiratoriaweb/static/js/runtime-main.860998d2.js"
+  }
+]);
